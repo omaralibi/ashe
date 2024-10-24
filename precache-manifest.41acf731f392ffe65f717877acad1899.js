2a54bbec610f7450e211f92fc71eb713a031d226
omar alibi <alibiomar3@gmail.com>
2024-10-24 22:06:50 +0100

    Updates

---
+++ precache-manifest.41acf731f392ffe65f717877acad1899.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6877321067dc42394b69aa6dde3eefc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b9cea79945b018c6ed2",
+    "url": "/static/css/2.32dbb16e.chunk.css"
+  },
+  {
+    "revision": "4df68d1e5ed1243c9d3f",
+    "url": "/static/css/main.d39fcc8d.chunk.css"
+  },
+  {
+    "revision": "5b9cea79945b018c6ed2",
+    "url": "/static/js/2.41825885.chunk.js"
+  },
+  {
+    "revision": "4df68d1e5ed1243c9d3f",
+    "url": "/static/js/main.68f704f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "05e473964a581c8b83e1cb9ac98386b5",
+    "url": "/static/media/Asset 1.05e47396.png"
+  },
+  {
+    "revision": "9f09c4d7b78cd468521323976451dccd",
+    "url": "/static/media/ena.9f09c4d7.jpg"
+  },
+  {
+    "revision": "c7522c9f905e3a5451e97310b615953d",
+    "url": "/static/media/logoblack.c7522c9f.png"
+  },
+  {
+    "revision": "37bf96cd894164f9da2358a9e784d624",
+    "url": "/static/media/logotiffany.37bf96cd.png"
+  },
+  {
+    "revision": "2600a8b7e2bd4ad19d7ebdb301a9460c",
+    "url": "/static/media/lone.2600a8b7.png"
+  }
+]);
